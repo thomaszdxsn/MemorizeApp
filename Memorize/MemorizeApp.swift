289e4036aa16a16b81bb60bf72de1f20dbc47b68
thomaszdxsn <thomaszdxsn@icloud.com>
2023-10-29 23:12:19 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 周杨 on 2023/10/29.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
